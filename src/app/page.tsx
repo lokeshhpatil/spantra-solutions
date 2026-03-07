0178146d70ab21e3d4a1b214c9dd397eb40166fe
lokeshpatil8975 <lokeshpatil8975@gmail.com>
2026-03-08 03:03:19 +0530

    feat: Add a new `Logos` component and integrate it into the home page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,38 +7,44 @@ import Services from "@/components/thirdpage/services";
 import Industries from "@/components/thirdpage/industries";
 import Heading from "@/components/fourth-page/heading";
 import Reviews from "@/components/fourth-page/Reviews";
+import Logos from "@/components/thirdpage/logos";
 // import Testimonials from "@/components/fourth-page/testimonials";
 
 export default function Home() {
   return (
-    <div>
-      {/* Hero Section takes remaining viewport height minus navbar (80px) */}
-      <Navbar />
-      <div className="flex flex-col lg:flex-row min-h-screen">
-        <FirstPart />
-        <SecondPart />
-      </div>
-
-      <div className="flex flex-col lg:flex-row min-h-screen">
-        <Firstpage />
-        <Secondpage />
-      </div>
-
-      {/* Third Form */}
-      <div className="flex flex-col lg:flex-row w-full min-h-screen">
-        <Services />
-        <Industries />
-      </div>
-
-      {/* Fourth Form */}
-      <div className="flex flex-col lg:flex-row min-h-[70vh] items-center px-6 md:px-12 lg:px-20 py-20 gap-10 lg:gap-20 bg-gray-50">
-        <div className="w-full lg:w-1/3 flex justify-center lg:justify-start">
-          <Heading />
+    <>
+      <div>
+        {/* Hero Section takes remaining viewport height minus navbar (80px) */}
+        <Navbar />
+        <div className="flex flex-col lg:flex-row min-h-screen">
+          <FirstPart />
+          <SecondPart />
         </div>
-        <div className="w-full lg:w-2/3">
-          <Reviews />
+
+        <div className="flex flex-col lg:flex-row min-h-screen">
+          <Firstpage />
+          <Secondpage />
+        </div>
+
+        {/* Third Form */}
+        <div className="flex flex-col lg:flex-row w-full min-h-screen">
+          <Services />
+          <Industries />
+        </div>
+
+        {/* Fourth Form */}
+        <div className="flex flex-col lg:flex-row min-h-[70vh] items-center px-6 md:px-12 lg:px-20 py-20 gap-10 lg:gap-20 bg-gray-50">
+          <div className="w-full lg:w-1/3 flex justify-center lg:justify-start">
+            <Heading />
+          </div>
+          <div className="md:flex w-full lg:w-2/3 mt-10">
+            <Reviews />
+            <div className="mt-10 md:justify-center items-center">
+              <Logos />
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
---
+++ src/components/thirdpage/logos.tsx
@@ -0,0 +1,47 @@
+const Logos = () => {
+  const logosNames = [
+    {
+      company: "google",
+      logoUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg",
+    },
+    {
+      company: "slack",
+      logoUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/b/b9/Slack_Technologies_Logo.svg",
+    },
+    {
+      company: "facebook",
+      logoUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/8/89/Facebook_Logo_%282019%29.svg",
+    },
+    {
+      company: "airbnb",
+      logoUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/6/69/Airbnb_Logo_B%C3%A9lo.svg",
+    },
+    {
+      company: "shopify",
+      logoUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/0/0e/Shopify_logo_2018.svg",
+    },
+  ];
+  return (
+    <div className="flex flex-wrap justify-center items-center gap-10">
+      {logosNames.map((item, index) => (
+        <div
+          key={index}
+          className="relative w-32 h-12 flex justify-center items-center"
+        >
+          <img
+            src={item.logoUrl}
+            alt={item.company}
+            className="w-full h-full object-contain"
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Logos;
